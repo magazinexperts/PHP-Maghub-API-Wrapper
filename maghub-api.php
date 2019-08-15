dc6f80f8759c7ab555c98ba15fc6687c3846eb52
Michael Ruth <mruth@magazinexperts.com>
2019-08-15 05:01:15 +0000

    first update in a while

--- maghub-api.php
+++ maghub-api.php
@@ -1,4 +1,6 @@
 <?php
+include_once( '../php_build_url.php');
+
 class maghub_api {
 	private $base_url;
 	private $private_key;
@@ -12,14 +14,16 @@ class maghub_api {
 	}
 
 
-	public function prepare_headers( $method, $endpoint, $resource_name ){
+	public function prepare_headers( $method, $endpoint, $resource_name, $extra_headers = array() ){
 		$this->headers	 = array();
 		$signature 		 = $this->build_signature( $method, $endpoint );
 		$accepts_header  = $this->build_accept_header( $resource_name );
 
 		$this->headers[] = $this->build_authorization_header( $signature );
 		$this->headers[] = "Accept: {$accepts_header}";
-
+		foreach( $extra_headers as $eh ){
+			$this->headers[] = $eh;
+		}
 		return $this->headers;
 	}
 
@@ -46,17 +50,28 @@ class maghub_api {
 		return $this->base_url . $endpoint;
 	}
 
-
-	public function get_request( $endpoint, $resource_name ){
+	public function get_request( $endpoint, $resource_name, $extra_headers = array(), $get = array() ){
 		$method = "GET";
-		$headers = $this->prepare_headers( $method, $endpoint, $resource_name );
+		
+		if(!empty($get)){
+			$endpoint_array = parse_url( $endpoint );
+			$endpoint_array['query'] = http_build_query( $get );
+			$endpoint = http_build_url($endpoint_array);
+		}
+
+		if(substr($endpoint,0,1) == "/"){
+			$endpoint = substr($endpoint,1);
+		}
+
+		$headers = $this->prepare_headers( $method, $endpoint, $resource_name, $extra_headers );
+		
 		//$signature = $this->build_signature( $endpoint, $resource_name );
 		//$accepts_header = $this->build_accept_header( $resource_name );
 		$uri = $this->build_uri( $endpoint );
 		//echo '<pre>'. print_r($headers,true) .'</pre>';
 		//echo "STARTING REQUEST FOR {$endpoint}:<br>\n";
 		$curl = curl_init();
-		curl_setopt_array($curl, array(
+		curl_setopt_array( $curl, array(
 		   CURLOPT_RETURNTRANSFER => 1,
 		   CURLOPT_URL => $uri,
 		   CURLOPT_HTTPHEADER => $headers,
@@ -84,9 +99,11 @@ class maghub_api {
 		return $results;
 	}
 
-	public function get_companies( $args = array() ){
+	
+	public function get_companies_by( $field, $value, $page = 0, $num_per_page = 10 ){
 		$available_args = array(
 			'company',
+			'company_id',
 			'active',
 			'org_id',
 			'organization',
@@ -94,7 +111,24 @@ class maghub_api {
 		
 		$endpoint = "companies";
 		$resource_name = "companies";
-		$results = $this->get_request( $endpoint, $resource_name );
+		$extra_headers = array( "X-OPT-LIMIT: {$num_per_page}", "X-OPT-OFFSET: {$page}" );
+		$results = $this->get_request( $endpoint, $resource_name, $extra_headers, array( $field => $value ));
+		return $results;
+	}
+	
+	public function get_companies( $page = 0, $num_per_page = 100, $args = array() ){
+		$available_args = array(
+			'company',
+			'company_id',
+			'active',
+			'org_id',
+			'organization',
+		);
+		
+		$endpoint = "companies";
+		$resource_name = "companies";
+		$extra_headers = array( "X-OPT-LIMIT: {$num_per_page}", "X-OPT-OFFSET: {$page}" );
+		$results = $this->get_request( $endpoint, $resource_name, $extra_headers );
 		return $results;
 	}
 
@@ -123,6 +157,12 @@ class maghub_api {
 	}
 	
 	function get_company_categories( $company_id ){
+		print_r($company_id);
+		if(is_array( $company_id )){
+			
+		}
+		
+		
 		$company_id = intval( $company_id );
 		$endpoint = "companies/{$company_id}/categories";
 		$resource_name = "company-categories";
@@ -140,7 +180,7 @@ class maghub_api {
 	
 	function get_company_category( $company_id, $category_id ){
 		$company_id = intval( $company_id );
-		$category_id = intval( $cocategory_idmpany_id );
+		$category_id = intval( $company_id );
 		$endpoint = "companies/{$company_id}/category{$category_id}";
 		$resource_name = "company-category";
 		$results = $this->get_request( $endpoint, $resource_name );
@@ -161,11 +201,83 @@ class maghub_api {
 		$results = $this->get_request( $endpoint, $resource_name );
 		return $results;
 	}
+	
+	function get_company_attribute_field_values( $company_id  ){
+		$company_id = intval( $company_id );
+		$endpoint = "companies/{$company_id}/attribute-values";
+		$resource_name = "company-attribute-values";
+		$results = $this->get_request( $endpoint, $resource_name );
+		return $results;
+	}
+	
+	function get_company_attribute_field_value( $company_id, $field_id ){
+		$company_id = intval( $company_id );
+		$field_id = intval( $field_id );
+		//https://magazinexperts.api.maghub.com/companies/68947/attribute-values/39
+		$endpoint = "companies/{$company_id}/attribute-values/{$field_id}";
+		$resource_name = "company-attribute-values";
+		$results = $this->get_request( $endpoint, $resource_name );
+		return $results;
+	}
 
-	public function get_contacts(){
+	public function get_contact_by( $field, $value, $page = 0, $num_per_page = 10 ){
+		$available_args = array(
+			'company',
+			'company_id',
+			'active',
+			'name',
+			'email',
+			'phone',
+		);
 		$endpoint = "contacts";
 		$resource_name = "contacts";
-		$results = $this->get_request( $endpoint, $resource_name );
+		$num_per_page = intval( $num_per_page );
+		$page = intval( $page );
+		$extra_headers = array( "X-OPT-LIMIT: {$num_per_page}", "X-OPT-OFFSET: {$page}" );
+		
+		$results = $this->get_request( $endpoint, $resource_name, $extra_headers, array( $field => $value ) );
+		return $results;
+	}
+	
+	
+	public function get_contacts( $args = array(), $page = 0, $num_per_page = 100 ){	
+		$endpoint = "contacts";
+		$resource_name = "contacts";
+		$num_per_page = intval( $num_per_page );
+		$page = intval( $page );
+		
+		$search_critera_args = array(
+			'company',
+			'company_id',
+			'active',
+			'name',
+			'email',
+			'phone'
+		);
+		
+		$filters = array();
+
+		if(!empty($args)){
+		foreach($args as $arg_key => $arg_value ){
+			if( in_array( $arg_key, $search_critera_args ) ){
+				switch( $arg_key ){
+					case "company_id":
+						$filters[$arg_key] = intval( $arg_value );
+					break;
+					case "phone":
+						$filters[$arg_key] = preg_replace('/[^0-9]/', '', $arg_value);
+					break;
+					default:
+						$filters[$arg_key] = $arg_value;
+					break;
+				}
+			}
+			
+		}
+		}
+		
+		$extra_headers = array( "X-OPT-LIMIT: {$num_per_page}", "X-OPT-OFFSET: {$page}" );
+		$results = $this->get_request( $endpoint, $resource_name, $extra_headers, $filters );
 		return $results;
 	}
 
@@ -194,6 +306,11 @@ class maghub_api {
 	}
 
 	public function get_publications(){
+		$available_args = array(
+			'name',
+			'type'
+		);
+		
 		$endpoint = "publications";
 		$resource_name = "publications";
 		$results = $this->get_request( $endpoint, $resource_name );
